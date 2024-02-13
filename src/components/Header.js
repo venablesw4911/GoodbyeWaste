bf07bc7700189c609c9e7f153798b433db48efa6
venablesw4911 <60434634+venablesw4911@users.noreply.github.com>
2024-02-12 23:21:25 -0600

    Aesthetic update
    - Worked on login ui
    - Added splash images

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom";
 
 export default function Header(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-primary col-12 h4 fixed-top" role="navigation">
+        <nav className="navbar navbar-expand-lg navbar-light bg-primary col-12 fixed-top header" role="navigation">
             <div className="container">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,9 +33,11 @@ export default function Header(props) {
                         <ul className="navbar-nav me-auto col-12 col-md-4">
                             <li className="nav-item my-auto ms-md-auto">
                                 <Link
-                                    className="link-underline link-offset-3 link-underline-opacity-0 link-underline-opacity-100-hover text-primary navbar-brand p-3"
-                                    to="/login">Log
-                                    In</Link>
+                                    className="h5 link-underline link-offset-3 link-underline-opacity-0 link-underline-opacity-100-hover text-primary p-3"
+                                    to="/login"
+                                >
+                                    Log In
+                                </Link>
                             </li>
                         </ul>
                     </div>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,23 @@
 import React from "react"
 import Carousel from 'react-bootstrap/Carousel';
 import {motion} from "framer-motion";
+import splash from "../assets/Landing-Splash.png"
 
 export default function Landing(props) {
     return (
         <motion.main
             className="min-vh-100 min-vw-100 container row"
-            initial={{width: 0}}
-            animate={{width: "100%"}}
+            initial={true}
+            animate={{x: 0}}
             exit={{x: -window.innerWidth, transition: {duration: 0.5}}}
+            style={{
+                backgroundImage: `url(${splash})`,
+                backgroundSize: "100% auto",
+            }}
         >
             <div className="col-sm-12 my-auto justify-content-center">
-                <div className="card bg-transparent border-0">
-                    <h1 className="text-center">
+                <div className="text-white card bg-transparent border-0">
+                    <h1 className="display-1 fw-bold text-center">
                         Welcome to
                     </h1>
                     <Carousel interval={3000} controls={false} indicators={false}>
