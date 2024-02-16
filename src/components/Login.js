9c3df559dbfe286e755b23f37c246784557b9136
grey-tucker <greytucker77@gmail.com>
2024-02-16 16:30:41 -0600

    Login page modifications

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,66 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Login(props) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [passwordError, setPasswordError] = useState("")
+    
+    const navigate = useNavigate();
+        
+    const onButtonClick = () => {
+        
+        setEmailError("");
+        setPasswordError("")
+
+        if ("" === email) {
+            setEmailError("Please enter your email")
+            return
+        }
+
+        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+            setEmailError("Please enter a valid email")
+            return
+        }
+
+        if ("" === password) {
+            setPasswordError("Please enter your password")
+            return
+        }
+
+        if (password.length < 10 || !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]{7,19}$/.test([password])) {
+            setPasswordError("The password must be 10 characters or longer")
+            return
+        }
+    }
+
     return (
         <form className="container">
             <div className="height-row mb-3 w-75 mx-auto">
-                <input type="email" className="form-control" id="email" placeholder="Email/Username"/>
+                <input 
+                    type="email" 
+                    className="form-control" 
+                    id="email" 
+                    placeholder="Email/Username"
+                    onChange={ev => setEmail(ev.target.value)}/>
+                <label className="height-row mb-3 w-75 mx-auto">{emailError}</label>
             </div>
             <div className="height-row mb-3 w-75 mx-auto">
-                <input type="password" className="form-control" id="password" placeholder="Password"/>
+                <input 
+                    type="password" 
+                    className="form-control" 
+                    id="password" 
+                    placeholder="Password"
+                    onChange={ev => setPassword(ev.target.value)}/>
+                <label className="height-row mb-3 w-75 mx-auto">{passwordError}</label>
             </div>
             <div className="height-row mb-3 w-75 mx-auto">
-                <button className="button-action bg-action text-white form-control">Log In</button>
+                <input 
+                className="button-action bg-action text-white form-control"
+                type="button"
+                onClick={onButtonClick}
+                value={"Log in"}/>
             </div>
             <div className="height-row mb-3 w-75 mx-auto text-start row">
                 <div className="col-md-6">
